11a11071dac539bc312e3ca17e7b018461770623
Iván <ivan_de_prada_apps1ta1920@cev.com>
2020-11-19 23:16:32 +0000

    Logic done

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var card1: UIButton!
+    @IBOutlet weak var card2: UIButton!
+    @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var endGame: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        initialSet()
     }
 
-
+    @IBAction func cardSelection(sender: UIButton){
+        let suma: Int = Int(sender.titleLabel!.text!)! + Int(number.text!)!
+        if (suma < 0 || suma > 21) {
+            endGame.text = "Has perdido"
+        }
+        number.text = String(suma)
+        
+        sender.setTitle(String(Int.random(in: -7..<7)), for: .normal)
+    }
+    
+    func initialSet(){
+        number.text = String(Int.random(in: 0..<20))
+        card1.setTitle(String(Int.random(in: -7..<7)), for: .normal)
+        card2.setTitle(String(Int.random(in: -7..<7)), for: .normal)
+    }
 }
 
