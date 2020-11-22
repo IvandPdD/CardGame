847dcf8c3af944d14973789299b8144c32f720f6
Iván <ivan_de_prada_apps1ta1920@cev.com>
2020-11-22 22:32:07 +0000

    Minor adjusts

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var card2: UIButton!
     @IBOutlet weak var number: UILabel!
 
+    //Al cargar la vista ejecuta la función explicada en la línea 46
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initialSet()
     }
     
+    //Acción para volver a establecer valores iniciales cuando se vuelve a la pantalla principal
     @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
         DispatchQueue.global(qos: .userInitiated).async {
             DispatchQueue.main.async {
@@ -26,7 +28,8 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    
+    //Acción para sleccionar uno de los números, sumarlos al principal y actualizar el número del botón con valor aleatorio. Si el número es menor o mayor a 21 se muestra la pantalla perdedora. Si el número es igual a 21 se muestra la pantalla ganadora.
     @IBAction func cardSelection(sender: UIButton){
         let suma: Int = Int(sender.titleLabel!.text!)! + Int(number.text!)!
         if (suma < 0 || suma > 21) {
@@ -40,6 +43,7 @@ class ViewController: UIViewController {
         sender.setTitle(String(Int.random(in: -5..<7)), for: .normal)
     }
     
+    //Función para establecer los valores iniciales del juego: números aleatorios para cada uno de los botones y el número principal
     func initialSet(){
         number.text = String(Int.random(in: 0..<20))
         card1.setTitle(String(Int.random(in: -5..<7)), for: .normal)
